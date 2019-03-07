d1762edfb9106475d2b8526311e7623e9d004909
Ipek Inan <kepinani1@gmail.com>
2019-03-07 09:56:09 -0500

    pokeapp

---
+++ client/build/precache-manifest.4aaff6f169e1f8dbfe7694f72d602e17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "753b79aad72f9eb22635",
+    "url": "/static/js/main.753b79aa.chunk.js"
+  },
+  {
+    "revision": "4f017cc511b124745b9a",
+    "url": "/static/js/2.4f017cc5.chunk.js"
+  },
+  {
+    "revision": "753b79aad72f9eb22635",
+    "url": "/static/css/main.3bc62e4f.chunk.css"
+  },
+  {
+    "revision": "b2087a855e0f365704c5e955c9348cc2",
+    "url": "/index.html"
+  }
+];
