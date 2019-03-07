5cf4244575c79a7d4279e223ad02728fc3c116f2
Ipek Inan <kepinani1@gmail.com>
2019-03-07 18:19:10 -0500

    new

--- client/src/components/MyCard.js
+++ client/src/components/MyCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import API from "../utils/API";
-import DeleteBtn from "../components/DeleteBtn";
+import DeleteBtn from "./DeleteBtn";
 // import {Button} from "react-bootstrap";
 // import {Link} from "react-router-dom";
 
