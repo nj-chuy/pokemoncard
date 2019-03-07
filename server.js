c74ea4b94322cf2334d5d7d64056099710ebdcb3
Ipek Inan <kepinani1@gmail.com>
2019-03-07 11:20:45 -0500

    new

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const app = express();
-// const path = require("path");
+const path = require("path");
 // const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes")
 // const booksRoutes = express.Router();
-
-// let Books = require("./models/book");
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(routes);
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
@@ -21,6 +13,18 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Book";
 
 mongoose.connect(MONGODB_URI);
 
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'client','build', 'index.html')); //relative path
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
