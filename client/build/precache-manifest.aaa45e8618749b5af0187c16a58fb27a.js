c64ab22301171fe5263c84555becee0379b70bfa
jesus garcia <nj.chuy@gmail.com>
2019-03-07 18:10:07 -0500

    removepkg

---
+++ client/build/precache-manifest.aaa45e8618749b5af0187c16a58fb27a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "96662e2e0d5f28211e69",
+    "url": "/static/js/main.96662e2e.chunk.js"
+  },
+  {
+    "revision": "9d3a2494882cb5a6aa61",
+    "url": "/static/js/2.9d3a2494.chunk.js"
+  },
+  {
+    "revision": "96662e2e0d5f28211e69",
+    "url": "/static/css/main.588126de.chunk.css"
+  },
+  {
+    "revision": "97bac37d0840dd94d31a0a67c07ff6a2",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes")
-// const booksRoutes = express.Router();
-
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Book";
@@ -14,17 +11,22 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Book";
 mongoose.connect(MONGODB_URI);
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use(routes);
 
-if(process.env.NODE_ENV==='production'){
+if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
   app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, 'client','build', 'index.html')); //relative path
   });
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
